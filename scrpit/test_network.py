ec5823c3c7300fb56d5d2f113b115ef00eb60846
ljh136 <66714630+ljh136@users.noreply.github.com>
2020-06-11 10:07:26 +0800

    添加一个4g信号

--- scrpit/test_network.py
+++ scrpit/test_network.py
@@ -24,3 +24,9 @@ class TestNetwork:
         self.networkPage.click_network()
         self.networkPage.click_first_network()
         self.networkPage.click_3g()
+        
+    def test_mobile_network_4g(self):
+        self.networkPage.click_more()
+        self.networkPage.click_network()
+        self.networkPage.click_first_network()
+        self.networkPage.click_3g()
